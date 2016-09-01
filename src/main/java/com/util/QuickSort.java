0993f75a98212dde7d57382ce325d761e5b2d394
Ptk <wb906565768@163.com>
2016-09-01 18:34:08 +0800

    QuickSort.java

---
+++ src/main/java/com/util/QuickSort.java
@@ -0,0 +1,41 @@
+package com.util;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		int[] array = { 8, 3, 5, 0, 9, 1, 6, 4, 2 };
+		SortUtil.printArray(array);
+
+		quick(array, 0, array.length - 1);
+		SortUtil.printArray(array);
+	}
+
+	private static void quick(int[] array, int left, int right) {
+		int middle = 0;
+		if (left < right) {
+			middle = middle(array, left, right);
+			quick(array, left, middle - 1);
+			quick(array, middle + 1, right);
+		}
+	}
+
+	private static int middle(int[] array, int left, int right) {
+		int temp = array[left];
+
+		while (left < right) {
+			while (left < right && array[right] <= temp) {
+				right--;
+			}
+			array[left] = array[right];
+
+			while (left < right && array[left] >= temp) {
+				left++;
+			}
+			array[right] = array[left];
+		}
+		array[left] = temp;
+		
+		return left;
+	}
+
+}
---
+++ src/main/java/com/util/SortUtil.java
@@ -0,0 +1,21 @@
+package com.util;
+
+public class SortUtil {
+
+	public static void printArray(int[] array) {
+		System.out.print("{");
+		for (int i = 0, l = array.length; i < l; i++) {
+			System.out.print(array[i]);
+			if (i < l - 1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println("}");
+	}
+
+	public static void main(String[] args) {
+		int[] array = { 1, 3, 4, 5 };
+		printArray(array);
+	}
+
+}
